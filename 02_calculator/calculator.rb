609d3a98e30cd38cec572a81668d6d98715ad9ab
Zachary White <zkwhited@gmail.com>
2017-04-20 13:42:49 -0600

    Finished 00 - 03

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,23 @@
-#write your code here
+def add(a, b)
+	a + b
+end
+
+def subtract(a, b)
+	a - b
+end
+
+def sum(numbers)
+	numbers.inject(0) { |total, a| total + a}
+end
+
+def multiply(*numbers)
+	numbers.reduce(1, :*)
+end
+
+def power(a, b)
+	a ** b
+end
+
+def factorial(a)
+	(1..a).inject(:*) || 1
+end
--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,31 @@
-#write your code here
+def echo(arg)
+	arg
+end
+
+def shout(arg)
+	arg.upcase
+end
+
+def repeat(a, times = 2) # use the = after an arg for default value
+	([a] * times).join(" ")
+end
+
+def start_of_word(word, a)
+	word[0..a-1]
+end
+
+def first_word(arg)
+	arg.split(" ")[0]
+end
+
+def titleize(arg)
+	words = arg.split.map do |word| #Break up arg into an array of word called "words"
+		if %w(the and over is of a).include?(word) # checks for exceptions to capitalizing
+			word
+		else
+			word.capitalize # capitlize first letter of word because not an exception
+		end
+	end
+	words.first.capitalize! #Capitalize first character regardless
+	words.join(" ") #join the array of modified split words (word)
+end
