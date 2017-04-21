3f9432441361db72958f2cb23b9d2d981e783ba8
Zachary White <zkwhited@gmail.com>
2017-04-21 14:04:07 -0600

    Finished rest. Commented solutions I needed help with.

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,31 @@
-#write your code here
+=begin Could get all but the last test
+
+def translate(word)
+	word_split = word.split(" ")
+
+	if word_split.size == 1
+		prefix = word[0, %w(a e i o u).map{|vowel| "#{word}aeiou".index(vowel)}.min]
+		"#{word[prefix.length..-1]}#{prefix}ay"
+	else
+		pig_words = []
+		word_split.each do |i|
+			pig_words.push(translate(i))
+		end
+		pig_words.join(" ")
+	end
+end
+
+=end
+
+#Found this solution on https://github.com/tempeste/learn_ruby/blob/master/04_pig_latin/pig_latin.rb
+# It's much more elegant than I came up with. I think I need to work on Regular Expressions more
+def translate(phrase) #splits up the phrase and passes each word into rearranage()
+  #Joins back into a phrase for return after rearrange
+  phrase.split(" ").map { |word| rearrange(word) }.join(" ") 
+end 
+
+def rearrange(word) #Set boolean if word meets criteria (match var)
+  match = word.match(/^((?:qu|[bcdfghjklmnpqrstvwxz])*)(.*)$/) #criteria defined by regex
+  # If regex criteria met, recombine the match data into the new word
+  match ? match[2] + match[1] + "ay" : word 
+end
--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,14 @@
 class Book
-# write your code here
+	attr_reader :title #We can use default reader 1 line
+	
+	def title=(str) #Instead of default accessor, use custom with properties
+		@title = titleize(str)
+	end
+
+	def titleize(str)
+		exceptions = %w{the a an and but or for nor in of} #define which words are not to be capitalized
+		#split into map function with index to ommit first words from exceptions, then capitalize everything else and join
+		str.split.each_with_index.map{|x, index| exceptions.include?(x) && index > 0 ? x : x.capitalize }.join(" ")
+		#the line is a bit too long for my taste
+	end
 end
